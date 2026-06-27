da69890ff66d96c8936821f6037b7853775561ee
Akshit Sharma <akshitsharma196@gmail.com>
2026-06-27 03:45:36 -0400

    bug fixes cleanup and canvas progress

--- races/Assets/Scripts/GameManager.cs
+++ races/Assets/Scripts/GameManager.cs
@@ -2,7 +2,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+
+    //UserInterface vars
+    [SerializeField] private Canvas MainGameCanvas;
+
+    //Widget vars
+    [SerializeField] private GameObject MainMenuWidget;
+    [serializeField] private GameObject GameOverWidget;
+
+    [serializeField] private GameObject PauseMenuWidget;
+
+    // Singleton instance
+    public static GameManager Instance { get; private set; }
+    void Awake()
+    {
+        if (FindObjectsOfType<GameManager>().Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        dontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
         
