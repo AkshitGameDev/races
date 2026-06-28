3f2c10418a1eb2ed6e7b2a02385e364b9ca7eaa9
Akshit Sharma <akshitsharma196@gmail.com>
2026-06-28 14:27:44 -0400

    making more guns

--- races/Assets/Scripts/GameManager.cs
+++ races/Assets/Scripts/GameManager.cs
@@ -4,33 +4,36 @@ public class GameManager : MonoBehaviour
 {
 
     //UserInterface vars
-    [SerializeField] private Canvas MainGameCanvas;
+    // [SerializeField] private Canvas MainGameCanvas;
 
-    //Widget vars
-    [SerializeField] private GameObject MainMenuWidget;
-    [serializeField] private GameObject GameOverWidget;
+    // //Widget vars
+    // [SerializeField] private GameObject MainMenuWidget;
+    // [serializeField] private GameObject GameOverWidget;
 
-    [serializeField] private GameObject PauseMenuWidget;
+    // [serializeField] private GameObject PauseMenuWidget;
 
-    // Singleton instance
-    public static GameManager Instance { get; private set; }
-    void Awake()
-    {
-        if (FindObjectsOfType<GameManager>().Length > 1)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-        dontDestroyOnLoad(gameObject);
-    }
-    void Start()
-    {
+    // //Game state vars
+    
+
+    // // Singleton instance
+    // public static GameManager Instance { get; private set; }
+    // void Awake()
+    // {
+    //     if (FindObjectsOfType<GameManager>().Length > 1)
+    //     {
+    //         Destroy(gameObject);
+    //         return;
+    //     }
+    //     Instance = this;
+    //     dontDestroyOnLoad(gameObject);
+    // }
+    // void Start()
+    // {
         
-    }
+    // }
 
-    void Update()
-    {
+    // void Update()
+    // {
         
-    }
+    // }
 }
