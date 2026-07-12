711b82c24f573ccb57ec60e2deee4960cc0ae830
Akshit Sharma <akshitsharma196@gmail.com>
2026-07-11 20:12:50 -0400

    Update AblityData.cs

--- races/Assets/Scripts/Ablity/AblityData.cs
+++ races/Assets/Scripts/Ablity/AblityData.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AblityData", menuName = "Scriptable Objects/AblityData")]
 public class AblityData : ScriptableObject
 {
+    public string label;
+    [ScerializeReference] public List<AblityEffect> effect;
+
+    void OnEnable()
+    {
+
+        if(string.IsNullOrEmpty(label))
+            label = name;
+        if (effect == null)
+            effect = new List<AblityEffect>();
+    }
     
 }
 
@@ -10,4 +21,17 @@ public class AblityData : ScriptableObject
 abstract class AblityEffect
 {
     public abstract void Execute(GameObject caster, GameObject target);
+}
+
+[Serializable]
+class DamageEffect : AblityEffect
+{
+    public float damageAmount;
+
+    public override void Execute(GameObject caster, GameObject target)
+    {
+        target.GetComponent<Health>().ApplyDamage(damageAmount);
+        // Implement damage logic here
+        Debug.Log($"{caster.name} deals {damageAmount} damage to {target.name}");
+    }
 }
